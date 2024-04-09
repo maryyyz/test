a067ed250c852319d23607148a7aa97ae42779c9
marmur <mashaskibams@gmail.com>
2024-04-09 20:07:21 +0300

    2Point

--- libs/data_structures/string_/string_.c
+++ libs/data_structures/string_/string_.c
@@ -1,3 +1,9 @@
-//
-// Created by mari on 06.04.24.
-//
+#include <stdio.h>
+
+size_t stringLength(const char *str) {
+    const char *ptr = str;
+    while (*ptr != '\0') {
+        ptr++;
+    }
+    return ptr - str;
+}
