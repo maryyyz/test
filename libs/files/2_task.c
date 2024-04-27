da03aeeb4215ae1620076b98a7d1d6480eee1088
marmur <mashaskibams@gmail.com>
2024-04-27 20:02:36 +0300

    2

---
+++ libs/files/2_task.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void convertFixedToFloat(const char *inputFileName, const char *outputFileName) {
+    FILE *inputFile = fopen(inputFileName, "r");
+    if (inputFile == NULL) {
+        printf("Failed to open input file\n");
+        return;
+    }
+
+    FILE *outputFile = fopen(outputFileName, "w");
+    if (outputFile == NULL) {
+        printf("Failed to open output file\n");
+        fclose(inputFile);
+        return;
+    }
+
+    double number;
+    while (fscanf(inputFile, "%lf", &number) == 1) {
+        fprintf(outputFile, "%.2f\n", number);
+    }
+
+    fclose(inputFile);
+    fclose(outputFile);
+}
+
+void testConvertFixedToFloat() {
+    FILE *testFile = fopen("test_fixed.txt", "w");
+    if (testFile == NULL) {
+        printf("Failed to create test file\n");
+        return;
+    }
+    fprintf(testFile, "3.14159\n2.71828\n0.123456\n");
+    fclose(testFile);
+
+    convertFixedToFloat("test_fixed.txt", "float_numbers.txt");
+
+    FILE *floatFile = fopen("float_numbers.txt", "r");
+    if (floatFile == NULL) {
+        printf("Failed to open transformed file\n");
+        return;
+    }
+
+    double number;
+    printf("Converted numbers:\n");
+    while (fscanf(floatFile, "%lf", &number) == 1) {
+        printf("%.2f\n", number);
+    }
+
+    fclose(floatFile);
+}
+
+int main() {
+    testConvertFixedToFloat();
+
+    return 0;
+}
